06cf4d7048af991030563021ed442eef3f141a1e
michaelkcwong <wong_michael@rogers.com>
2023-03-16 23:07:45 -0400

    Show a loading indicator

--- quotes.js
+++ quotes.js
@@ -1,8 +1,13 @@
 const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random'
+const spinner = document.querySelector('#js-spinner');
 
 async function getQuote() {
+  // remove the "hidden" class on the spinner
+  spinner.classList.remove('hidden');
+  //disable the quote button
+  newQuoteButton.disabled = true;
 
   try {
     const response = await fetch(endpoint)
@@ -14,6 +19,11 @@ async function getQuote() {
   } catch (err) {
     console.log(err)
     alert('Failed to fetch new quote');
+  } finally {
+    // enable the quote button
+    newQuoteButton.disabled = false;
+    // add the "hidden" class back again
+    spinner.classList.add('hidden');
   }
 }
 
